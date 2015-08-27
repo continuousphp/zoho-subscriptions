ac0aa9291789c183608803a1cc78a94678464b98
ppaulis <pascal.paulis@continuousphp.com>
2015-08-27 13:07:12 +0200

    fixed phpunit config

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,74 +1,24 @@
 <?php
-/**
- * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2015 Julien Guittard (http://julienguittard.com)
- */
 
-namespace ZohoTest\Subscriptions;
-
-use Zend\Loader\AutoloaderFactory;
-use RuntimeException;
+namespace ZohoTest;
 
 error_reporting(E_ALL | E_STRICT);
 chdir(__DIR__);
+date_default_timezone_set("Europe/Brussels");
 
-/**
- * Class Bootstrap
- *
- * @package ZohoTest\Subscriptions
- */
 class Bootstrap
 {
-    protected static $serviceManager;
+    protected static $config;
+    protected static $bootstrap;
 
     public static function init()
     {
         static::initAutoloader();
     }
 
-
     protected static function initAutoloader()
     {
-        $vendorPath = static::findParentPath('vendor');
-
-        if (is_readable($vendorPath . '/autoload.php')) {
-            $loader = include $vendorPath . '/autoload.php';
-            return;
-        }
-
-        $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
-
-        if (!$zf2Path) {
-            throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
-        }
-
-        if (isset($loader)) {
-            $loader->add('Zend', $zf2Path . '/Zend');
-        } else {
-            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-            AutoloaderFactory::factory(array(
-                'Zend\Loader\StandardAutoloader' => array(
-                    'autoregister_zf' => true,
-                    'namespaces' => array(
-                        'ZF\Apigility\Admin' => __DIR__ . '/../src/',
-                        __NAMESPACE__ => __DIR__,
-                        'Test' => __DIR__ . '/../vendor/Test/',
-                    ),
-                ),
-            ));
-        }
-    }
-
-    protected static function findParentPath($path)
-    {
-        $dir = __DIR__;
-        $previousDir = '.';
-        while (!is_dir($dir . '/' . $path)) {
-            $dir = dirname($dir);
-            if ($previousDir === $dir) return false;
-            $previousDir = $dir;
-        }
-        return $dir . '/' . $path;
+        require_once(__DIR__ . '/../vendor/autoload.php');
     }
 }
 
