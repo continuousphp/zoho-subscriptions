6cf6de4802547e147fd32a1bf14537596bb6bd7b
ppaulis <pascal.paulis@continuousphp.com>
2015-08-31 18:32:53 +0200

    added support for access token

--- config/module.config.php
+++ config/module.config.php
@@ -22,7 +22,6 @@ return [
         ],
     ],
     'zoho' => [
-        'webhook_access_token' => 'xd1ze61fefa3f1afvaz5v1gvzagv321za56f1zaf',
         'ssl_config' => [
             'sslcapath' => '/etc/ssl/certs'
         ],
