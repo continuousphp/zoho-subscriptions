b26eec7abd69b900929e401e265dc06f5329e3fb
ppaulis <pascal.paulis@continuousphp.com>
2016-01-27 11:20:23 +0100

    added handling for subscription activation

--- tests/ZohoTest/Service/Webhook/SubscriptionTest.php
+++ tests/ZohoTest/Service/Webhook/SubscriptionTest.php
@@ -409,6 +409,24 @@ class SubscriptionTest extends TestCase
         $this->assertTrue($eventTriggered);
     }
 
+    public function testTriggerSubscriptionEvent_SubscriptionActivated()
+    {
+        $eventTriggered = false;
+        $subscriptionData = $this->subscriptionDataCreated;
+        $subscriptionData['event_type'] = 'subscription_activation';
+
+        $this->instance->getEventManager()->getSharedManager()->attach(
+            'subscriptionWebhook', 'subscriptionActivated', function (\Zend\EventManager\Event $e) use (&$eventTriggered, $subscriptionData)
+        {
+            $eventTriggered = true;
+            $this->assertEquals($e->getParam('subscription'), $subscriptionData['data']['subscription']);
+        });
+
+        $this->instance->triggerSubscriptionEvent($subscriptionData);
+
+        $this->assertTrue($eventTriggered);
+    }
+
     public function testTriggerSubscriptionEvent_SubscriptionRenewed()
     {
         $eventTriggered = false;
